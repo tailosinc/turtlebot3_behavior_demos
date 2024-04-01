314a8dcb37332bce717838192991ed22f5e9b0fa
Justin Nguyen <justin@maidbot.com>
2024-04-01 13:03:35 -0500

    Add go_back_home function

--- docker/functions.sh
+++ docker/functions.sh
@@ -2,6 +2,10 @@
 
 # Function to teleop the bot (manually control/drive the bot around using keyboard inputs)
 function teleop {
-    ros2 run teleop_twist_keyboard teleop_twist_keyboard
+  ros2 run teleop_twist_keyboard teleop_twist_keyboard
+}
+
+function go_back_home {
+  ros2 action send_goal /navigate_to_pose nav2_msgs/action/NavigateToPose "{pose: {header: {frame_id: "map"}, pose: {position: {x: 0.0, y: 0.0, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: 0.0, w: 1.0}}}}"
 }
 
