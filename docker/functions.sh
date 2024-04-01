58aadc1400b315458aa4d6ad0d45943de182bfe7
natali-maidbot <natali.salas-espana@tailos.com>
2024-04-01 14:23:43 -0500

    add helper fxn for launching node

--- docker/functions.sh
+++ docker/functions.sh
@@ -10,3 +10,13 @@ function go_back_home {
   ros2 action send_goal /navigate_to_pose nav2_msgs/action/NavigateToPose "{pose: {header: {frame_id: "map"}, pose: {position: {x: 0.0, y: 0.0, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: 0.0, w: 1.0}}}}"
 }
 
+# Function to call demo script with red, green, or blue parameter.
+function navigate_until_color_is_found
+{
+  local color_arg=$1
+  if [ $# -eq 0 ]; then
+    color_arg=blue
+  fi
+  ros2 launch tb3_autonomy tb3_demo_behavior_py.launch.py target_color:="${color_arg}" location_file:=$HOME/my_waypoints.yaml
+}
+
